fd419aef510db6579d5ac800c94d9d53b2845683
SEkaterina <katerina-kozyrenko@yandex.ru>
2024-06-08 17:04:03 +0300

    исправлена валидация формы подписки

--- src/scripts/formSubscribe.js
+++ src/scripts/formSubscribe.js
@@ -3,21 +3,19 @@ import { checkFormContacts } from './validate.js';
 export const initFormSubscribe = () => {
   const form = document.querySelector('.subscribe__form');
   const validator = checkFormContacts(form);
+  const div = document.createElement('div');
 
   form.addEventListener('submit', async (e) => {
     e.preventDefault();
 
     const isValid = await validator.revalidate();
+    div.remove();
 
     if (isValid) {
-      const div = document.createElement('div');
       div.classList.add('success-subscribe');
-      div.textContent = 'Ваша заявка принята';
+      div.textContent = 'Вы подписались на рассылку';
       form.append(div);
       form.reset();
-    } else {
-      const div = document.querySelector('.success-subscribe');
-      div ? div.remove() : '';
     }
   });
 }
